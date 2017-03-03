ed2c02eb0abbe422530792bb435d97ffdc346dd8
Robert Basic <robertbasic.com@gmail.com>
2017-03-03 18:37:00 +0100

    CreateAccount command handler

---
+++ tests/PerFiUnitTest/Domain/Account/CommandHandler/CreateAcount.php
@@ -0,0 +1,35 @@
+<?php
+declare(strict_types=1);
+
+namespace PerFiUnitTest\Domain\Account\CommandHandler;
+
+use PHPUnit\Framework\TestCase;
+use PerFi\Application\Account\InMemoryAccountRepository;
+use PerFi\Domain\Account\CommandHandler\CreateAccount;
+use PerFi\Domain\Account\Command\CreateAccount as CreateAccountCommand;
+
+class CreateAccountTest extends TestCase
+{
+    /**
+     * @test
+     */
+    public function when_invoked_it_adds_the_account_to_the_accounts_repository()
+    {
+        $type = 'asset';
+        $title = 'Cash';
+
+        $repository = new InMemoryAccountRepository();
+
+        $commandHandler = new CreateAccount($repository);
+
+        $command = new CreateAccountCommand($type, $title);
+
+        $commandHandler->__invoke($command);
+
+        $result = $repository->getAll();
+
+        $expected = 1;
+
+        self::assertSame($expected, count($result));
+    }
+}
