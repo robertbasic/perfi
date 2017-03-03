10282e59181c7136a4f7411f8731cb87fb5fb5d5
Robert Basic <robertbasic.com@gmail.com>
2017-03-03 16:29:24 +0100

    Account entity

---
+++ src/PerFi/Domain/Account/Account.php
@@ -0,0 +1,62 @@
+<?php
+declare(strict_types=1);
+
+namespace PerFi\Domain\Account;
+
+use PerFi\Domain\Account\AccountId;
+use PerFi\Domain\Account\AccountType;
+use Ramsey\Uuid\Uuid;
+use Webmozart\Assert\Assert;
+
+class Account
+{
+    /**
+     * @var AccountId
+     */
+    private $id;
+
+    /**
+     * @var AccountType
+     */
+    private $type;
+
+    /**
+     * @var string
+     */
+    private $title;
+
+    private function __construct(AccountId $id, AccountType $type, string $title)
+    {
+        $this->id = $id;
+        $this->type = $type;
+        $this->title = $title;
+    }
+
+    public static function byType(AccountType $type, string $title) : self
+    {
+        Assert::stringNotEmpty($title);
+
+        $id = AccountId::fromUuid(Uuid::uuid4());
+
+        return new self(
+            $id,
+            $type,
+            $title
+        );
+    }
+
+    public function title()
+    {
+        return $this->title;
+    }
+
+    public function type()
+    {
+        return $this->type;
+    }
+
+    public function __toString()
+    {
+        return sprintf("%s, %s", $this->title(), $this->type());
+    }
+}
---
+++ tests/PerFiUnitTest/Domain/Account/AccountTest.php
@@ -0,0 +1,36 @@
+<?php
+declare(strict_types=1);
+
+namespace PerFiUnitTest\Domain\Account;
+
+use PHPUnit\Framework\TestCase;
+use PerFi\Domain\Account\Account;
+use PerFi\Domain\Account\AccountType;
+
+class AccountTest extends TestCase
+{
+    /**
+     * @test
+     */
+    public function account_can_be_created_by_type_with_title()
+    {
+        $type = AccountType::fromString('asset');
+        $title = 'Cash';
+
+        $account = Account::byType($type, $title);
+
+        self::assertSame('Cash, asset', (string) $account);
+    }
+
+    /**
+     * @test
+     * @expectedException InvalidArgumentException
+     */
+    public function account_cannot_be_created_with_empty_title()
+    {
+        $type = AccountType::fromString('asset');
+        $title = '';
+
+        $account = Account::byType($type, $title);
+    }
+}
