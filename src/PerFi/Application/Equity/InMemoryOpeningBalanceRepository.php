9ef65f85dd603bbab271f8164332e518d1265bfd
Robert Basic <robertbasic.com@gmail.com>
2017-03-03 11:38:23 +0100

    Test calculating totals

--- src/PerFi/Application/Equity/InMemoryOpeningBalanceRepository.php
+++ src/PerFi/Application/Equity/InMemoryOpeningBalanceRepository.php
@@ -39,7 +39,7 @@ class InMemoryOpeningBalanceRepository implements OpeningBalanceRepository
                 }
             }
 
-            $totals[] = $total;
+            $totals[$currency] = $total;
         }
 
 
--- tests/PerFiUnitTest/Application/Equity/InMemoryOpeningBalanceRepositoryTest.php
+++ tests/PerFiUnitTest/Application/Equity/InMemoryOpeningBalanceRepositoryTest.php
@@ -15,13 +15,13 @@ class InMemoryOpeningBalanceRepositoryTest extends TestCase
      */
     public function adding_opening_balance_added_per_currency()
     {
+        $repository = new InMemoryOpeningBalanceRepository();
+
         $amount = '500';
         $currency = 'RSD';
         $money = MoneyFactory::amountInCurrency($amount, $currency);
-
         $openingBalance = OpeningBalance::forStarting($money);
 
-        $repository = new InMemoryOpeningBalanceRepository();
         $repository->add($openingBalance);
 
         $balances = $repository->getALl();
@@ -33,6 +33,49 @@ class InMemoryOpeningBalanceRepositoryTest extends TestCase
                 $balanceAmount = $balance->value();
                 self::assertEquals($amount, $balanceAmount);
             }
+        }
+    }
+
+    /**
+     * @test
+     */
+    public function totals_are_calculated_per_currency()
+    {
+        $repository = new InMemoryOpeningBalanceRepository();
+
+        $amount = '500';
+        $currency = 'RSD';
+        $money = MoneyFactory::amountInCurrency($amount, $currency);
+        $openingBalance = OpeningBalance::forStarting($money);
+
+        $repository->add($openingBalance);
+
+        $amount = '600';
+        $currency = 'RSD';
+        $money = MoneyFactory::amountInCurrency($amount, $currency);
+        $openingBalance = OpeningBalance::forStarting($money);
+
+        $repository->add($openingBalance);
+
+        $amount = '60';
+        $currency = 'EUR';
+        $money = MoneyFactory::amountInCurrency($amount, $currency);
+        $openingBalance = OpeningBalance::forStarting($money);
+
+        $repository->add($openingBalance);
+
+        $totals = $repository->getTotals();
+
+        $expected = [
+            'RSD' => '1100',
+            'EUR' => '60',
+        ];
+
+        foreach ($totals as $currency => $total) {
+            $expectedTotal = MoneyFactory::amountInCurrency($expected[$currency], $currency);
+
+            self::assertArrayHasKey($currency, $expected);
+            self::assertTrue($expectedTotal->equals($total));
         }
     }
 }
