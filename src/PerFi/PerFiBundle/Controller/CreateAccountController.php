c1084f2049ecec68fb8e6bdecc74c1054e8beaea
Robert Basic <robertbasic.com@gmail.com>
2017-03-11 17:39:01 +0100

    Handle create account command after the form is submitted

--- src/PerFi/PerFiBundle/Controller/CreateAccountController.php
+++ src/PerFi/PerFiBundle/Controller/CreateAccountController.php
@@ -3,6 +3,7 @@ declare(strict_types=1);
 
 namespace PerFi\PerFiBundle\Controller;
 
+use PerFi\Domain\Account\Command\CreateAccount;
 use PerFi\PerFiBundle\Form\AccountType;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
@@ -18,6 +19,19 @@ class CreateAccountController extends Controller
     public function indexAction(Request $request)
     {
         $form = $this->createForm(AccountType::class, []);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $formData = $form->getData();
+
+            $type = $formData['type'];
+            $title = $formData['title'];
+
+            $command = new CreateAccount($type, $title);
+
+            $this->get('command_bus')->handle($command);
+        }
 
         return [
             'form' => $form->createView(),
