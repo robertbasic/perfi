b8c680100c62af60c547ff046f1b50249015589f
Robert Basic <robertbasic.com@gmail.com>
2017-03-11 12:53:46 +0100

    Tie in the menu item listener

---
+++ src/PerFi/PerFiBundle/EventListener/MenuItemListener.php
@@ -0,0 +1,49 @@
+<?php
+declare(strict_types=1);
+
+namespace PerFi\PerFiBundle\EventListener;
+
+use Avanzu\AdminThemeBundle\Event\SidebarMenuEvent;
+use Avanzu\AdminThemeBundle\Model\MenuItemModel;
+use Symfony\Component\HttpFoundation\Request;
+
+class MenuItemListener
+{
+    public function onSetupMenu(SidebarMenuEvent $event)
+    {
+        $request = $event->getRequest();
+
+        foreach ($this->getMenu($request) as $item) {
+            $event->addItem($item);
+        }
+    }
+
+    protected function getMenu(Request $request)
+    {
+        $menuItems = array(
+            $homepage = new MenuItemModel('homepage', 'Home', 'homepage', [], 'iconclasses fa fa-home'),
+            $accounts = new MenuItemModel('accounts', 'Accounts', null, [], 'iconclasses fa fa-bank'),
+        );
+
+        $accounts->addChild(
+            new MenuItemModel('create_account', 'Create account', 'create_account', [], 'iconclasses fa fa-plus')
+        );
+
+        return $this->activateByRoute($request->get('_route'), $menuItems);
+    }
+
+    protected function activateByRoute($route, $items)
+    {
+        foreach($items as $item) {
+            if($item->hasChildren()) {
+                $this->activateByRoute($route, $item->getChildren());
+            } else {
+                if($item->getRoute() == $route) {
+                    $item->setIsActive(true);
+                }
+            }
+        }
+
+        return $items;
+    }
+}
