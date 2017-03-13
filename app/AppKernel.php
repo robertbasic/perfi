bcd78ca5d05059ab325f0fbe3f43f3bfa8c90c84
Robert Basic <robertbasic.com@gmail.com>
2017-03-13 16:51:18 +0100

    No need for the serializer after all

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -16,7 +16,6 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new Symfony\Bundle\AsseticBundle\AsseticBundle(),
-            new JMS\SerializerBundle\JMSSerializerBundle(),
             new Avanzu\AdminThemeBundle\AvanzuAdminThemeBundle(),
             new SimpleBus\SymfonyBridge\SimpleBusCommandBusBundle(),
             new PerFi\PerFiBundle\PerFiBundle(),
