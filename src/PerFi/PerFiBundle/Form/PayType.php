59659c284570000a5b521ed2389c247f8601d27c
Robert Basic <robertbasic.com@gmail.com>
2017-03-13 19:52:28 +0100

    Asset and expense accounts from repository
    Build choices of asset and expense accounts
    from the account repository.

--- src/PerFi/PerFiBundle/Form/PayType.php
+++ src/PerFi/PerFiBundle/Form/PayType.php
@@ -3,6 +3,8 @@ declare(strict_types=1);
 
 namespace PerFi\PerFiBundle\Form;
 
+use PerFi\Domain\Account\AccountRepository;
+use PerFi\Domain\Account\AccountType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\NumberType;
@@ -11,16 +13,26 @@ use Symfony\Component\Form\FormBuilderInterface;
 
 class PayType extends AbstractType
 {
+    /**
+     * @var AccountRepository
+     */
+    private $accountRepository;
+
+    public function __construct(AccountRepository $repository)
+    {
+        $this->accountRepository = $repository;
+    }
+
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
             ->add('source', ChoiceType::class, [
                 'required' => true,
-                'choices' => [],
+                'choices' => $this->getAssetAccounts(),
             ])
             ->add('destination', ChoiceType::class, [
                 'required' => true,
-                'choices' => []
+                'choices' => $this->getExpenseAccounts(),
             ])
             ->add('amount', NumberType::class, [
                 'required' => true,
@@ -32,5 +44,34 @@ class PayType extends AbstractType
             ->add('description', TextType::class, [
                 'required' => true,
             ]);
+    }
+
+    private function getAssetAccounts() : array
+    {
+        $type = AccountType::fromString('asset');
+
+        $assets = $this->accountRepository->getAllByType($type);
+
+        return $this->buildAccountChoices($assets);
+    }
+
+    private function getExpenseAccounts() : array
+    {
+        $type = AccountType::fromString('expense');
+
+        $expenses = $this->accountRepository->getAllByType($type);
+
+        return $this->buildAccountChoices($expenses);
+    }
+
+    private function buildAccountChoices(array $accounts) : array
+    {
+        $choices = [];
+
+        foreach ($accounts as $account) {
+            $choices[$account->title()] = (string) $account->id();
+        }
+
+        return $choices;
     }
 }
