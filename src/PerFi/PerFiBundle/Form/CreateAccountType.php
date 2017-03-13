88975e06b7415b136fceb3bf8fbfb0ab647bc7b9
Robert Basic <robertbasic.com@gmail.com>
2017-03-13 09:31:13 +0100

    Rename form to CreateAccountType

--- src/PerFi/PerFiBundle/Form/CreateAccountType.php
+++ src/PerFi/PerFiBundle/Form/CreateAccountType.php
@@ -9,7 +9,7 @@ use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 
-class AccountType extends AbstractType
+class CreateAccountType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
