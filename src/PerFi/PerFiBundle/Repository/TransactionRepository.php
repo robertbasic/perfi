f1889e2734d2a897c35089ed2163f951065c485e
Robert Basic <robertbasic.com@gmail.com>
2017-03-14 08:14:37 +0100

    Save a transaction entity to the database

--- src/PerFi/PerFiBundle/Repository/TransactionRepository.php
+++ src/PerFi/PerFiBundle/Repository/TransactionRepository.php
@@ -5,6 +5,7 @@ namespace PerFi\PerFiBundle\Repository;
 use Doctrine\ORM\EntityRepository;
 use PerFi\Domain\Transaction\Transaction;
 use PerFi\Domain\Transaction\TransactionRepository as TransactionRepositoryInterface;
+use PerFi\PerFiBundle\Entity\Transaction as DtoTransaction;
 
 /**
  * TransactionRepository
@@ -17,6 +18,21 @@ class TransactionRepository extends EntityRepository
      */
     public function add(Transaction $transaction)
     {
+        $entity = new DtoTransaction();
+        $entity->setTransactionId((string) $transaction->id());
+        $entity->setType((string) $transaction->type());
+        $entity->setSourceAccount((string) $transaction->sourceAccount()->id());
+        $entity->setDestinationAccount((string) $transaction->destinationAccount()->id());
+        $entity->setDate($transaction->date());
+        $entity->setDescription($transaction->description());
+
+        $amount = $transaction->amount();
+        $entity->setAmount($amount->getAmount());
+        $entity->setCurrency((string) $amount->getCurrency());
+
+        $em = $this->getEntityManager();
+        $em->persist($entity);
+        $em->flush();
     }
 
     /**
