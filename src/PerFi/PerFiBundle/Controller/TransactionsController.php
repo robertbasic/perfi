da105a64ba600c5fca166a0fd29dc5dd443c5c81
Robert Basic <robertbasic.com@gmail.com>
2017-03-17 11:14:02 +0100

    Setup datatables for transaction log

--- src/PerFi/PerFiBundle/Controller/TransactionsController.php
+++ src/PerFi/PerFiBundle/Controller/TransactionsController.php
@@ -5,6 +5,7 @@ namespace PerFi\PerFiBundle\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -25,5 +26,7 @@ class TransactionsController extends Controller
      */
     public function listAction(Request $request)
     {
+        return new JsonResponse([
+        ]);
     }
 }
