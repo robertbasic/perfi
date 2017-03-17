51485f6b43bcb194d27fc9eef2246437c0ef1595
Robert Basic <robertbasic.com@gmail.com>
2017-03-17 06:45:18 +0100

    Add transaction logs page

---
+++ src/PerFi/PerFiBundle/Controller/TransactionsController.php
@@ -0,0 +1,29 @@
+<?php
+declare(strict_types=1);
+
+namespace PerFi\PerFiBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+
+class TransactionsController extends Controller
+{
+    /**
+     * @Template
+     * @Route("/transactions", name="transactions")
+     */
+    public function indexAction(Request $request)
+    {
+        return [
+        ];
+    }
+
+    /**
+     * @Route("/transactions-list", name="transactions_list")
+     */
+    public function listAction(Request $request)
+    {
+    }
+}
