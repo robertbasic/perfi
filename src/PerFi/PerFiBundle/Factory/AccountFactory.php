382482d1b57f109df0a528a4cdbdc95a1c507224
Robert Basic <robertbasic.com@gmail.com>
2017-03-19 16:52:36 +0100

    Transaction and account factories
    Factories to create Transaction and Account
    objects from database results.

---
+++ src/PerFi/PerFiBundle/Factory/AccountFactory.php
@@ -0,0 +1,26 @@
+<?php
+declare(strict_types=1);
+
+namespace PerFi\PerFiBundle\Factory;
+
+use PerFi\Domain\Account\Account;
+use PerFi\Domain\Account\AccountId;
+use PerFi\Domain\Account\AccountType;
+
+class AccountFactory
+{
+    /**
+     * Create an Account from a row in the database
+     *
+     * @param array $account
+     * @return Account
+     */
+    public static function fromArray(array $account) : Account
+    {
+        return Account::withId(
+            AccountId::fromString($account['id']),
+            AccountType::fromString($account['type']),
+            $account['title']
+        );
+    }
+}
---
+++ src/PerFi/PerFiBundle/Factory/TransactionFactory.php
@@ -0,0 +1,53 @@
+<?php
+declare(strict_types=1);
+
+namespace PerFi\PerFiBundle\Factory;
+
+use PerFi\Domain\MoneyFactory;
+use PerFi\Domain\Transaction\Transaction;
+use PerFi\Domain\Transaction\TransactionDate;
+use PerFi\Domain\Transaction\TransactionId;
+use PerFi\Domain\Transaction\TransactionRecordDate;
+use PerFi\Domain\Transaction\TransactionType;
+use PerFi\PerFiBundle\Factory\AccountFactory;
+
+class TransactionFactory
+{
+    /**
+     * Create a Transaction from a row in the database
+     *
+     * @param array $transaction
+     * @return Transaction
+     */
+    public static function fromArray(array $transaction) : Transaction
+    {
+        $sourceAccount = AccountFactory::fromArray(self::getSubArray($transaction, 'source_account'));
+        $destinationAccount = AccountFactory::fromArray(self::getSubArray($transaction, 'destination_account'));
+
+        return Transaction::withId(
+            TransactionId::fromString($transaction['transaction_id']),
+            TransactionType::fromString($transaction['type']),
+            $sourceAccount,
+            $destinationAccount,
+            MoneyFactory::centsInCurrency($transaction['amount'], $transaction['currency']),
+            TransactionDate::fromString($transaction['date']),
+            TransactionRecordDate::fromString($transaction['record_date']),
+            $transaction['description']
+        );
+    }
+
+    private static function getSubArray(array $array, string $prefix) : array
+    {
+        $subArray = [];
+
+        foreach ($array as $key => $value) {
+            if (strpos($key, $prefix) === false) {
+                continue;
+            }
+
+            $subArray[substr($key, strlen($prefix) + 1)] = $value;
+        }
+
+        return $subArray;
+    }
+}
