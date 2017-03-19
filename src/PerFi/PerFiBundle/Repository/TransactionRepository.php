b38d1ec77e20b21f946fbd285c9a1312dc05b29f
Robert Basic <robertbasic.com@gmail.com>
2017-03-19 17:22:58 +0100

    Reusable query parts

--- src/PerFi/PerFiBundle/Repository/TransactionRepository.php
+++ src/PerFi/PerFiBundle/Repository/TransactionRepository.php
@@ -42,24 +42,32 @@ class TransactionRepository extends EntityRepository
      */
     public function getAll() : array
     {
-        $qb = $this->getEntityManager()->getConnection()->createQueryBuilder();
+        $query = $this->getQuery();
 
-        $statement = $qb->select(
-            't.transaction_id', 't.type', 't.amount', 't.currency',
-            't.date', 't.record_date', 't.description',
-            'sa.account_id AS source_account_id',
-            'sa.title AS source_account_title',
-            'sa.type AS source_account_type',
-            'da.account_id AS destination_account_id',
-            'da.title AS destination_account_title',
-            'da.type AS destination_account_type'
-        )
-        ->from('transaction', 't')
-        ->innerJoin('t', 'account', 'sa', 't.source_account = sa.account_id')
-        ->innerJoin('t', 'account', 'da', 't.destination_account = da.account_id')
-        ->execute();
+        $statement = $query->execute();
 
         return $this->mapToEntities($statement);
+    }
+
+    private function getQuery()
+    {
+        $qb = $this->getEntityManager()->getConnection()->createQueryBuilder();
+
+        $query = $qb->select(
+                't.transaction_id', 't.type', 't.amount', 't.currency',
+                't.date', 't.record_date', 't.description',
+                'sa.account_id AS source_account_id',
+                'sa.title AS source_account_title',
+                'sa.type AS source_account_type',
+                'da.account_id AS destination_account_id',
+                'da.title AS destination_account_title',
+                'da.type AS destination_account_type'
+            )
+            ->from('transaction', 't')
+            ->innerJoin('t', 'account', 'sa', 't.source_account = sa.account_id')
+            ->innerJoin('t', 'account', 'da', 't.destination_account = da.account_id');
+
+        return $query;
     }
 
     private function mapToEntities($statement) : array
