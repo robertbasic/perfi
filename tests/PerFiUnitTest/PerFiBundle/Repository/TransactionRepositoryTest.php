0851ad4cf74de6bf3f07fba350f23c301fae5d93
Robert Basic <robertbasic.com@gmail.com>
2017-03-20 07:14:55 +0100

    Test getting all transactions

--- tests/PerFiUnitTest/PerFiBundle/Repository/TransactionRepositoryTest.php
+++ tests/PerFiUnitTest/PerFiBundle/Repository/TransactionRepositoryTest.php
@@ -136,4 +136,67 @@ class TransactionRepositoryTest extends TestCase
 
         $this->repository->add($this->transaction);
     }
+
+    /**
+     * @test
+     * @dataProvider transactions
+     */
+    public function can_get_all_transactions_from_repository($transactions)
+    {
+        $this->queryBuilder->shouldReceive('select')
+            ->once()
+            ->with(
+                't.transaction_id', 't.type', 't.amount', 't.currency',
+                't.date', 't.record_date', 't.description',
+                'sa.account_id AS source_account_id',
+                'sa.title AS source_account_title',
+                'sa.type AS source_account_type',
+                'da.account_id AS destination_account_id',
+                'da.title AS destination_account_title',
+                'da.type AS destination_account_type'
+            )
+            ->andReturnSelf();
+        $this->queryBuilder->shouldReceive('from')
+            ->once()
+            ->with('transaction', 't')
+            ->andReturnSelf();
+        $this->queryBuilder->shouldReceive('innerJoin')
+            ->once()
+            ->with('t', 'account', 'sa', 't.source_account = sa.account_id')
+            ->andReturnSelf();
+        $this->queryBuilder->shouldReceive('innerJoin')
+            ->once()
+            ->with('t', 'account', 'da', 't.destination_account = da.account_id')
+            ->andReturnSelf();
+
+        $this->statement->shouldReceive('fetch')
+            ->andReturnUsing(function() use (&$transactions) { return array_pop($transactions); });
+
+        $result = $this->repository->getAll();
+
+        self::assertInstanceOf(Transaction::class, array_pop($result));
+    }
+
+    public function transactions()
+    {
+        return [
+            [
+                [[
+                    'transaction_id' => 'fddf4716-6c0e-4f54-b539-d2d480a50d1c',
+                    'type' => 'pay',
+                    'source_account_id' => 'fddf4716-6c0e-4f54-b539-d2d480a50d1a',
+                    'source_account_type' => 'asset',
+                    'source_account_title' => 'Cash',
+                    'destination_account_id' => 'fddf4716-6c0e-4f54-b539-d2d480a50d1b',
+                    'destination_account_type' => 'expense',
+                    'destination_account_title' => 'Groceries',
+                    'amount' => '50000',
+                    'currency' => 'RSD',
+                    'date' => '2017-03-12',
+                    'record_date' => '2017-03-20 06:55:00',
+                    'description' => 'supermarket',
+                ]]
+            ]
+        ];
+    }
 }
