943a931be31b8a8d96eca6eaac139fa588c6bbc9
Robert Basic <robertbasic.com@gmail.com>
2017-03-30 08:55:37 +0200

    Use traits for in memory repository tests

--- tests/PerFiUnitTest/Application/Transaction/InMemoryTransactionRepositoryTest.php
+++ tests/PerFiUnitTest/Application/Transaction/InMemoryTransactionRepositoryTest.php
@@ -4,38 +4,19 @@ declare(strict_types=1);
 namespace PerFiUnitTest\Application\Transaction;
 
 use PHPUnit\Framework\TestCase;
+use PerFiUnitTest\Traits\TransactionTrait;
 use PerFi\Application\Transaction\InMemoryTransactionRepository;
-use PerFi\Domain\Account\Account;
-use PerFi\Domain\Account\AccountType;
-use PerFi\Domain\MoneyFactory;
-use PerFi\Domain\Transaction\Transaction;
-use PerFi\Domain\Transaction\TransactionDate;
-use PerFi\Domain\Transaction\TransactionType;
 
 class InMemoryTransactionRepositoryTest extends TestCase
 {
+    use TransactionTrait;
+
     /**
      * @test
      */
     public function can_add_transaction_to_repository()
     {
-        $type = TransactionType::fromString('pay');
-        $asset = AccountType::fromString('asset');
-        $expense = AccountType::fromString('expense');
-        $source = Account::byTypeWithTitle($asset, 'Cash');
-        $destination = Account::byTypeWithTitle($expense, 'Groceries');
-        $amount = MoneyFactory::amountInCurrency('500', 'RSD');
-        $date = TransactionDate::fromString('2017-03-12');
-        $description = 'groceries for dinner';
-
-        $transaction = Transaction::betweenAccounts(
-            $type,
-            $source,
-            $destination,
-            $amount,
-            $date,
-            $description
-        );
+        $transaction = $this->payTransaction();
 
         $repository = new InMemoryTransactionRepository();
 
